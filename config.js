bba45440333f29788c40edc083b44b9bcee4008f
arafura-store <eugendinescu@wwwarafura.com>
2026-04-15 16:18:03 +0300

    Use publishable key for browser

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Configuratie conexiune baza de date
 window.SUPABASE_URL = 'https://bxsfzfnpejkmwxkuoshb.supabase.co';
-(function(){var p=['sb_','secr','et_V','88l8','Um7W','-Tok','CzHo','ZQ1v','Q_Do','rwNG','wt'];window.SUPABASE_KEY=p.join('');})();
+window.SUPABASE_KEY = 'sb_publishable_naQ6WD91NngRNjTlV5fSJw_BInIyurw';
